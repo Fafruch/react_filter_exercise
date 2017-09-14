b31e517acaf0304c7b7e671c8e77e840386bb173
Fafruch <fafruszek@gmail.com>
2017-09-14 19:11:44 +0200

    Provide solution for First, Second and Third steps
    This commit also adds prop-types module to dependencies and updates
    redux and react-redux.

--- src/people/Filter.jsx
+++ src/people/Filter.jsx
@@ -3,30 +3,31 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-// ...
+import { setFilterQuery } from '../actions/people'
 
 class Filter extends Component {
   static propTypes = {
-    // ...
+    filterQuery: PropTypes.string.isRequired,
+    setFilterQuery: PropTypes.func.isRequired
   };
 
-  // ...
-
   render() {
+    const { filterQuery } = this.props;
+
     return (
       <div className='App-box'>
-        {/* ... */}
+        <input value={filterQuery} onChange={(event) => this.props.setFilterQuery(event.target.value)} />
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  // ...
+  filterQuery: state.people.filterQuery
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-  // ...
+  setFilterQuery
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Filter);
--- src/people/People.jsx
+++ src/people/People.jsx
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-// ...
 
 class People extends Component {
   static propTypes = {
-    // ...
+    people: PropTypes.array.isRequired,
+    filterQuery: PropTypes.string.isRequired
   };
 
-  // ...
+  renderPeople = () => {
+    const { people, filterQuery } = this.props;
+    const regexp = new RegExp(filterQuery, 'i');
+
+    const filteredPeople = people.filter((person) => person.name.match(regexp));
+
+    return filteredPeople.map((person) => (
+      <div className='App-box' key={person.id}>
+        {person.name}
+      </div>
+    ))
+  }
 
   render() {
     return (
       <div>
-        {/* ... */}
+        {this.renderPeople()}
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  // ...
+  people: state.people.people,
+  filterQuery: state.people.filterQuery
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-  // ...
-}, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(People);
+export default connect(mapStateToProps)(People);
